44d7150b7cb0636c784fa8248084ba3b099a7cc7
Antonio Vitor <mod.vtr@gmail.com>
2024-07-29 16:06:47 -0300

    Primeira classe: Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import jogotabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Posicao pos = new Posicao (3, 5);
+		System.out.println(pos);
 
 	}
 
